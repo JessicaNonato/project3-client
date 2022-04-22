f64671309d1f32ddec30bd5c41f3f10aa5afd0de
Juliana Madeira <juliwood1980@gmail.com>
2022-04-22 16:01:13 -0300

    styled components Subscribe

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,22 +1,107 @@
 import React from "react";
-import { renderIntoDocument } from "react-dom/test-utils";
-import '../styles/subscribe.css'
+import styled from 'styled-components';
+
+const SubsDiv = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Ms+Madi&display=swap');
+    background-color: #F9DCDF;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+
+    @media screen and (max-width: 768px) {
+        background-color: #F9DCDF;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+    }
+`
+
+const SubsText = styled.p`
+    font-family: 'Ms Madi';
+    font-size: 80px;
+    color: white;
+    padding: 0;
+    margin: 0;
+    @media screen and (max-width: 768px){
+        font-family: 'Ms Madi', cursive;
+        font-size: 40px;
+        color: white;
+        padding: 0;
+        margin: 0;
+    }
+`
+const SubsDivText = styled.div`
+`
+const SubsForm = styled.form`
+`
+const SubsDivForm = styled.div`
+`
+
+const InputForm = styled.input`
+    border: 1px solid #F9DCDF ;
+    border-radius: 20px;
+    padding: 14px;
+    margin-right: 10px;
+    color: lightgray;
+    width: 250px;
+    @media screen and (max-width: 768px){
+        border: 1px solid #F9DCDF ;
+        border-radius: 20px;
+        padding: 5px;
+        margin-right: 5px;
+        margin-bottom: 5px;
+        color: lightgray;
+        width: 180px;
+    }
+`
+const ButtonForm = styled.button`
+    border: 1px solid white;
+    border-radius: 20px;
+    background-color: white;
+    color: #F9DCDF;
+    font-family: 'Ms Madi';
+    font-size: 25px;
+    padding: 0px 20px;
+
+    &:hover{
+        background-color: #F9DCDF;
+        color: white;
+        }; 
+
+    @media screen and (max-width: 768px){
+        border: 1px solid white;
+        border-radius: 20px;
+        background-color: white;
+        color: #F9DCDF;
+        font-family: 'Ms Madi', cursive;
+        font-size: 18px;
+        padding: 0px 20px;
+
+        &:hover{
+        background-color: #F9DCDF;
+        color: white;
+        }; 
+    }
+`
+
 
 const Subscribe = () => {
 
     return(
-        <div className="subs">
-            <div>
-            <p className="subscribe">Subscribe</p>
-            </div>
-            <div>
-                <form>
-                <input type="text" value='e-mail adress' className="input-form"></input>
-                <button className="button-form">Send</button>
-                </form>
-            </div>
+        <SubsDiv>
+            <SubsDivText>
+            <SubsText>Subscribe</SubsText>
+            </SubsDivText>
+            <SubsDivForm>
+                <SubsForm>
+                <InputForm type="text" placeholder="your e-mail here"></InputForm>
+                <ButtonForm>Send</ButtonForm>
+                </SubsForm>
+            </SubsDivForm>
 
-        </div>
+        </SubsDiv>
     )
 }
 
